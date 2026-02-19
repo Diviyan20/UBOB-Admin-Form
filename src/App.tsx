73f8840256e751b2c7c9024d1716ce47ddc5ea8a
Diviyan Rajan <diviyanr@gmail.com>
2026-02-19 15:27:35 +0800

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AdminLoginForm from './views/AdminLoginForm'
+
+function App() {
+  return (
+    <>
+      <div>
+        <AdminLoginForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/AdminLoginForm.tsx
@@ -0,0 +1,10 @@
+
+export default function AdminLoginForm(){
+    return(
+        <>
+        <div>
+            <h1>Login as Admin</h1>
+        </div>
+        </>
+    );
+};
