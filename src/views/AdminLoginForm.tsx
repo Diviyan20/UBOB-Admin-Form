015375dec7fb84e524673bfc4c6d118a3874c46e
Diviyan Rajan <diviyanr@gmail.com>
2026-03-05 09:53:16 +0800

    UI Adjustments

--- src/views/AdminLoginForm.tsx
+++ src/views/AdminLoginForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styling/AdminLoginStyles.css"
+import logo from '../assets/Logo.png'
 
 
 const SERVER_URL = "http://localhost:5000";
@@ -56,6 +57,7 @@ export default function AdminLoginForm() {
     return (
         <div className="container">
             <div className="form-wrapper">
+                <img src={logo} alt="Logo" className="login-logo"/>
                 <h2>Login as Admin</h2>
 
                 {error && <div className="error">{error}</div>}
