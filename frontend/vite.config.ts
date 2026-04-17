efd1c419b0c2958e6bcc421def5978cef77ac4b5
Diviyan Rajan <diviyanr@gmail.com>
2026-04-17 17:13:19 +0800

    Modify vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   define:{
     'process.env.VITE_ORDER_TRACKING_URL':JSON.stringify(process.env.VITE_ORDER_TRACKING_URL)
   }
