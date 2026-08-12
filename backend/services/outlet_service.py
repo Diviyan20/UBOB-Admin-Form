60bb120ee8c12773b02d8fa8fc9702cd9cc1333e
Diviyan Rajan <diviyanr@gmail.com>
2026-08-12 19:27:08 +0800

    Rearrage directory and fix CORS policy

---
+++ backend/services/outlet_service.py
@@ -0,0 +1,81 @@
+import os
+from ast import List
+from pathlib import Path
+
+import requests
+from flask import jsonify
+from models.active_outlets import get_all_outlets
+
+CACHE_ROOT = Path(os.getenv("CACHE_ROOT", "/tmp/digital-signage-cache"))
+CACHE_DIR = CACHE_ROOT / "outlets"
+CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+# -----------------
+# CONFIG
+# -----------------
+ODOO_DATABASE_URL = os.getenv("ODOO_DATABASE_URL")
+API_TOKEN = os.getenv("API_TOKEN")
+PUBLIC_HOST = os.getenv("PUBLIC_HOST_URL")
+
+HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json",
+}
+
+# HEADERS
+def odoo_headers():
+    return{
+        "Authorization":f"Bearer {API_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+# ===============================
+# FETCH ALL OUTLET INFORMATION
+# ===============================
+def fetch_all_outlet_data() -> List:
+    """
+    Main function which fetches all the outlet data from Odoo.
+    Returns complete information of all outlets
+    """
+    try:
+        response = requests.post(
+            f"{ODOO_DATABASE_URL}/api/get/outlet/regions",
+            json={"ids":[]},
+            headers=odoo_headers(),
+            timeout=15
+        )
+        
+        response.raise_for_status()
+        
+        data = response.json()
+        
+        if not data.get("status"):
+            return jsonify({"erro": "Odoo API Error"}), []
+        
+        else:
+            # Extract all outlet data
+            outlets = []
+            for region in data.get("data",[]):
+                region_name = region.get("outlet_region_name")
+                
+                for outlet in region.get("pos_shops",[]):
+                    outlets.append({
+                        "outlet_id": str(outlet.get("id")),
+                        "outlet_name": str(outlet.get("name")),
+                        "region_name": region_name,
+                        "is_open": outlet.get("is_open",False)
+                    })
+            return outlets
+    except Exception as e:
+        return jsonify({"error": str(e)}),[]
+
+def fetch_registered_outlets() -> list:
+    """
+    Fetches all registered outlets from the database.
+    Returns a list of dictionaries containing outlet information.
+    """
+    try:
+        outlets = get_all_outlets()
+        return outlets
+    except Exception as e:
+        raise ValueError(f"Error fetching registered outlets: {e}")
