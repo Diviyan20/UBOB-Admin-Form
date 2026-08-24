d398cc9b3cdc8cf358b17970183a066f81aa9b57
Diviyan Rajan <diviyanr@gmail.com>
2026-08-24 10:56:21 +0800

    Implement Menu Component with navigable menu items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,45 @@
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import AdminLoginForm from './views/forms/AdminLoginForm'
-import ConfigurationForm from './views/forms/ConfigurationForm'
-import OutletDashboard from './views/dashboards/OutletDashboard'
-import OutletScreenConfiguration from './components/OutletScreenConfiguration'
-import MediaLibrary from './components/MediaLibrary'
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+import AdminLoginForm from "./views/forms/AdminLoginForm";
+import ConfigurationForm from "./views/forms/ConfigurationForm";
+import OutletDashboard from "./views/dashboards/OutletDashboard";
+import OutletScreenConfiguration from "./views/dashboards/OutletScreenConfiguration";
+import MediaLibrary from "./views/dashboards/MediaLibrary";
+import MenuBar from "./components/menu/MenuComponent";
+
+function AdminLayout() {
+  return (
+    <>
+      <MenuBar />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to="/login"/>}></Route>
-      <Route path="/login" element={<AdminLoginForm/>}></Route>
-      <Route path="/configuration" element={<ConfigurationForm/>}></Route>
-      <Route path="/outlet-dashboard" element={<OutletDashboard/>}></Route>
-      <Route path="/outlet-screen" element={<OutletScreenConfiguration/>}></Route>
-      <Route path="/media-library" element={<MediaLibrary/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />}></Route>
+        <Route path="/login" element={<AdminLoginForm />}></Route>
+        <Route element={<AdminLayout />}>
+          <Route path="/configuration" element={<ConfigurationForm />}></Route>
+          <Route path="/outlet-dashboard" element={<OutletDashboard />}></Route>
+          <Route
+            path="/outlet-screen"
+            element={<OutletScreenConfiguration />}
+          ></Route>
+          <Route path="/media-library" element={<MediaLibrary />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
